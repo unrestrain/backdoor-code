344f958f2a1679bcf58693ad593ad5f0189d0e49
unrestrain <67731060+unrestrain@users.noreply.github.com>
2022-04-21 14:08:34 +0800

    Update meta_detection.py

--- defense/meta_detection/meta_detection.py
+++ defense/meta_detection/meta_detection.py
@@ -1,8 +1,8 @@
 import numpy as np
 import torch
 import torch.utils.data
-from utils_meta import load_model_setting, epoch_meta_train, epoch_meta_eval
-from meta_classifier import MetaClassifier
+from utils_meta import load_model_setting, epoch_meta_train, epoch_meta_eval, epoch_meta_train_oc,epoch_meta_eval_oc
+from meta_classifier import MetaClassifier,MetaClassifierOC
 import argparse
 from tqdm import tqdm
 
@@ -30,3 +30,20 @@ def meta_detect(meta_model, target_model):
   if result > 0.5:
     return True
   return False
+
+
+def train_meta_model_oneclass(meta_model,shadow_model,trainset,testset,epoch=10,save_path=None):
+    '''
+    :param meta_model: meta_model
+    :param shadow_model: model structure
+    :param trainset: list, each element is 
+    '''
+    optimizer = torch.optim.Adam(meta_model.parameters(), lr=1e-3)
+    for i in range(epoch):
+        print(('epoch:%s'%i).center(50,'='))
+        epoch_meta_train_oc(meta_model, shadow_model, optimizer, train_dataset, is_discrete=False)
+        auc,acc = epoch_meta_eval_oc(meta_model,shadow_model,testset,is_discrete=False,threshold=0.5)
+        print('test acc: %s'%acc)
+
+    if save_path:
+        torch.save(meta_model, save_path)
