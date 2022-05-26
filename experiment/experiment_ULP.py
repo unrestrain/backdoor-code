2630c13a8dd80886be4b261925aa1ae64c08c06d
unrestrain <67731060+unrestrain@users.noreply.github.com>
2022-05-26 13:09:09 +0800

    Update experiment_ULP.py

--- experiment/experiment_ULP.py
+++ experiment/experiment_ULP.py
@@ -49,5 +49,27 @@ def detect_cifar10_resnet18_model(trigger_type, device='cuda:0'):
     print(trigger_type.center(100,'='))
 
 
+def detect_paper_original(device='cuda:0'):
+    # poisoned
+    poisoned_models_train = sorted(glob.glob('./poisoned_models/trainval/*.pt'))[:400]
+    poisoned_models_val = sorted(glob.glob('./poisoned_models/trainval/*.pt'))[400:]
+
+    # clean models
+    clean_models=glob.glob('./clean_models/trainval/*.pt')
+
+    # train - 400 clean 400 poisoned
+    models_train=clean_models[:400] + poisoned_models_train
+    labels_train=np.concatenate([np.zeros((len(clean_models[:400]),)),np.ones((len(poisoned_models_train),))])
+
+    # val - 100 clean 100 poisoned
+    models_val=clean_models[400:] + poisoned_models_val
+    labels_val=np.concatenate([np.zeros((len(clean_models[400:]),)),np.ones((len(poisoned_models_val),))])
+
+
+    ulp = ULP(N=10, W=32, H=32, nofclasses=10,device=device,C=3)
+    ulp.train(train_datas=models_train, train_labels=labels_train, val_datas=models_val,val_labels=labels_val, epochs=1500,device=device)
+    ulp.save('my_ulp.pkl')
+
 if __name__ == '__main__':
     detect_cifar10_resnet18_model(trigger_type='white square', device='cuda:1')
+    # detect_paper_original(device='cuda:1')
